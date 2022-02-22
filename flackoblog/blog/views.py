253e34f344011acd80de781d0c7c974433a6f94c
rjhelm <rjhelm@github.io>
2022-02-22 12:27:52 -0700

    updated views to target post and home templates

--- flackoblog/blog/views.py
+++ flackoblog/blog/views.py
@@ -1,3 +1,17 @@
+from re import template
 from django.shortcuts import render
+from .models import posts
+from django.views import generic
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
 
-# Create your views here.
+# class based views for posts
+class postslist(generic.ListView):
+    queryset = posts.object.filter(status=1).order_by('-created_on')
+    template_name = 'home.html'
+    paginate_by = 4
+    
+# class based views for each post
+class postdetail(generic.DetailView):
+    model = posts
+    template_name = 'post.html'
