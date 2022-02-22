50f7e59e3e931b249c510e01e22cd13412d7c45c
rjhelm <rjhelm@github.io>
2022-02-22 13:52:48 -0700

    created cms and site now functions and displays content || more to come

--- flackoblog/blog/views.py
+++ flackoblog/blog/views.py
@@ -1,17 +1,10 @@
-from re import template
-from django.shortcuts import render
-from .models import posts
 from django.views import generic
-from django.views.decorators.http import require_GET
-from django.http import HttpResponse
+from .models import Post
 
-# class based views for posts
-class postslist(generic.ListView):
-    queryset = posts.object.filter(status=1).order_by('-created_on')
-    template_name = 'home.html'
-    paginate_by = 4
-    
-# class based views for each post
-class postdetail(generic.DetailView):
-    model = posts
-    template_name = 'post.html'
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
--- flackoblog/flackoblog/settings.py
+++ flackoblog/flackoblog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +30,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -52,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'flackoblog.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'template')
 
 TEMPLATES = [
     {
